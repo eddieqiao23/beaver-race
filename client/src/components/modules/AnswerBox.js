f1054807ed86574aff48d1f80cbdd52fc3711a75
Eddie Qiao <eddieqiao@gmail.com>
2024-01-17 18:39:01 -0500

    scuffed indiv zetamac done

---
+++ client/src/components/modules/AnswerBox.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./AnswerBox.css";
+
+
+// Page that displays all elements of a multiplayer race 
+const Question = (props) => {
+    const 
+
+    return (
+        <div className="AnswerBox-container">
+            <input type="text" placeholder="" />
+        </div>
+    )
+}
+
+export default Question;
